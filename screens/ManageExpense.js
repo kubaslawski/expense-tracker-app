d8b259b8008a82b7ce350201faf3b818a5c48aec
kubaslawski <kubaslawski.it@gmail.com>
2022-08-31 15:26:57 +0200

    11. ManageExpense screen - delete button.

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,9 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+// components
+import IconButton from "../components/ui/IconButton";
+// constants 
+import { globalStyles } from "../constants/styles";
 
 const ManageExpense = ({ navigation, route }) => {
     const editedExpenseId = route.params?.expenseId;
@@ -12,10 +16,34 @@ const ManageExpense = ({ navigation, route }) => {
     }, [navigation, isEditing]);
 
     return (
-        <Text>
-            ManageExpense Screen
-        </Text>
+        <View style={styles.container}>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton
+                        icon="trash"
+                        color={globalStyles.colors.error500}
+                        size={36}
+                        onPress={() => { }}
+                    />
+                </View>
+            )}
+        </View>
     )
 };
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: globalStyles.colors.primary800,
+        flex: 1,
+        padding: 24,
+    },
+    deleteContainer: {
+        alignItems: "center",
+        borderTopColor: globalStyles.colors.primary200,
+        borderTopWidth: 2,
+        marginTop: 16,
+        paddingTop: 8,
+    }
+});
