9ca1cdf1955fca0b053c60a589d7d96cdba107e6
kubaslawski <kubaslawski.it@gmail.com>
2022-09-05 12:54:47 +0200

    19. Styling Form component.

--- components/manage_expense/ExpenseForm.js
+++ components/manage_expense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 // components 
 import Input from './Input';
 
@@ -7,24 +7,37 @@ const ExpenseForm = () => {
     const handleAmmountChange = () => {
 
     };
+
     const handleDateChange = () => {
 
     };
+
     const handleDescriptionChange = () => {
 
     };
 
     return (
-        <View>
-            <Input label="Ammount" textInputConfig={{
-                keyboardType: "decimal-pad",
-                onChangeText: handleAmmountChange,
-            }} />
-            <Input label="Date" textInputConfig={{
-                placeholder: "YYYY-MM-DD",
-                maxLenght: 10,
-                onChangeText: handleDateChange,
-            }} />
+        <View style={styles.form}>
+            <Text style={styles.title}>Your Expense</Text>
+            <View style={styles.inputsRow}>
+                <Input
+                    label="Ammount"
+                    style={styles.rowInput}
+                    textInputConfig={{
+                        keyboardType: "decimal-pad",
+                        onChangeText: handleAmmountChange,
+                    }}
+                />
+                <Input
+                    label="Date"
+                    style={styles.rowInput}
+                    textInputConfig={{
+                        placeholder: "YYYY-MM-DD",
+                        maxLenght: 10,
+                        onChangeText: handleDateChange,
+                    }}
+                />
+            </View>
             <Input label="Description" textInputConfig={{
                 multiline: true,
                 onChangeText: handleDescriptionChange,
@@ -34,3 +47,23 @@ const ExpenseForm = () => {
 };
 
 export default ExpenseForm;
+
+const styles = StyleSheet.create({
+    form: {
+        marginTop: 40,
+    },
+    title: {
+        color: "white",
+        fontSize: 24,
+        fontWeight: "bold",
+        marginVertical: 24,
+        textAlign: "center",
+    },
+    inputsRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    rowInput: {
+        flex: 1,
+    }
+});
