22ba56600d097b907439c8e49c745d077ca31807
kubaslawski <kubaslawski.it@gmail.com>
2022-09-07 12:38:01 +0200

    32. Basic validators.

---
+++ util/validators/validators.js
@@ -0,0 +1,8 @@
+export const isEmpty = (string) => {
+    return string.trim() === "";
+};
+
+export const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return email.match(regEx);
+};
